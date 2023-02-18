5dcb8b57a5343e3d6d46cccdd60c956e0c645caa
Brzzzn <brzzzn2005@gmail.com>
2023-02-18 22:23:14 +0100

    fix

--- src/main/java/brzzzn/fabadditions/registry/FabBlocks.java
+++ src/main/java/brzzzn/fabadditions/registry/FabBlocks.java
@@ -1,11 +1,8 @@
 package brzzzn.fabadditions.registry;
 
 import brzzzn.fabadditions.FabAdditions;
-import brzzzn.fabadditions.block.Kiln;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
-import net.minecraft.block.Material;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
@@ -14,9 +11,6 @@ import net.minecraft.util.registry.Registry;
 
 public class FabBlocks
 {
-    public static final Block KILN = registerBlock("kiln",
-            new Kiln(FabricBlockSettings.of(Material.METAL).strength(0.3f, 0.3f)), ItemGroup.DECORATIONS);
-
     private static Block registerBlockWithoutItem(String name, Block block)
     {
         return Registry.register(Registry.BLOCK, new Identifier(FabAdditions.ID, name), block);
