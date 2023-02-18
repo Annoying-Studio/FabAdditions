a6dbcbb62289b79593d081eeade5f188b7986913
Tidari <52622026+Till1805@users.noreply.github.com>
2023-02-18 22:32:01 +0100

    Implemented Kotlin

--- src/main/kotlin/brzzzn/fabadditions/FabAdditions.kt
+++ src/main/kotlin/brzzzn/fabadditions/FabAdditions.kt
@@ -1,21 +1,19 @@
-package brzzzn.fabadditions;
+package brzzzn.fabadditions
 
-import brzzzn.fabadditions.registry.FabBlocks;
-import brzzzn.fabadditions.registry.FabItems;
-import net.fabricmc.api.ModInitializer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import brzzzn.fabadditions.registry.FabBlocks
+import brzzzn.fabadditions.registry.FabItems
+import net.fabricmc.api.ModInitializer
+import org.slf4j.LoggerFactory
 
-public class FabAdditions implements ModInitializer {
+class FabAdditions : ModInitializer {
+    override fun onInitialize() {
+        FabItems.registerModItems()
+        FabBlocks.registerModBlocks()
+    }
 
-	public static final String ID = "fabadditions";
+    companion object {
+        const val ID = "fabadditions"
 
-	public static final Logger LOGGER = LoggerFactory.getLogger("fabadditions");
-
-	@Override
-	public void onInitialize()
-	{
-		FabItems.registerModItems();
-		FabBlocks.registerModBlocks();
-	}
+		val logger = LoggerFactory.getLogger("fabadditions")
+    }
 }
