ae8201ab8e646cb25e85fb66fb4f74e3427ae701
Tidari <52622026+Till1805@users.noreply.github.com>
2023-02-20 21:33:21 +0100

    Started work on PhantomStaff (/tpa (bukkit) equivalent)

---
+++ src/main/kotlin/brzzzn/fabadditions/registry/FabItemsRegistry.kt
@@ -0,0 +1,57 @@
+package brzzzn.fabadditions.registry
+
+import brzzzn.fabadditions.FabAdditions
+import brzzzn.fabadditions.FabAdditions.Companion.logger
+import brzzzn.fabadditions.item.AmethystFeather
+import brzzzn.fabadditions.item.InterdimensionalFeather
+import brzzzn.fabadditions.item.PhantomStaff
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings
+import net.minecraft.item.Item
+import net.minecraft.item.ItemGroup
+import net.minecraft.util.Identifier
+import net.minecraft.util.registry.Registry
+
+object FabItemsRegistry {
+
+    //region default settings
+    private val defaultToolSettings: FabricItemSettings = FabricItemSettings()
+            .group(ItemGroup.TOOLS)
+            .maxCount(1)
+    //endregion
+
+    /**
+     * Registers a simple item as a mod item
+     */
+    private fun registerItem(name: String, item: Item): Item {
+        return Registry.register(Registry.ITEM, Identifier(FabAdditions.ID, name), item)
+    }
+
+    /**
+     * Registers all modded items
+     */
+    fun registerModItems() {
+        logger.debug("Registering Mod Items for ${FabAdditions.ID}")
+        // Call all registers
+        registerTools()
+
+        logger.debug("Done registering Mod Items for ${FabAdditions.ID}")
+    }
+
+    private fun registerTools() {
+        // Amethyst Feather
+        registerItem(
+                "amethyst_feather",
+                AmethystFeather(defaultToolSettings)
+        )
+        // Interdimensional Feather
+        registerItem(
+                "interdimensional_feather",
+                InterdimensionalFeather(defaultToolSettings)
+        )
+        // Phantom staff
+        registerItem(
+                "phantom_staff",
+                PhantomStaff(defaultToolSettings)
+        )
+    }
+}
