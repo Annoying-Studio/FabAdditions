3ef7b9b8617b35c02287079727f2668262ac9a2a
Tidari <52622026+Tidari@users.noreply.github.com>
2023-08-17 20:41:50 +0200

    The Phantom staff now asks the target player if it's ok to teleport to them

--- src/main/kotlin/brzzzn/fabadditions/data/PlayerRef.kt
+++ src/main/kotlin/brzzzn/fabadditions/data/PlayerRef.kt
@@ -1,7 +1,13 @@
 package brzzzn.fabadditions.data
 
+import net.minecraft.entity.player.PlayerEntity
+import net.minecraft.server.network.ServerPlayerEntity
+
 data class PlayerRef(
     val name: String,
     val uuid: String
+) {
+    constructor(player: PlayerEntity) : this(player.name.string, player.uuidAsString)
 
-)
+    constructor(player: ServerPlayerEntity) : this(player.name.string, player.uuidAsString)
+}
